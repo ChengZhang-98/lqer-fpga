59b84f561829c030475aad84e46d1f60e1ae76fc
Cheng ZHANG <chengzhang98@outlook.com>
2024-02-29 15:02:21 +0000

    add skid buffer

--- hardware/user/components/common/rtl/skid_buffer.sv
+++ hardware/user/components/common/rtl/skid_buffer.sv
@@ -1,8 +1,39 @@
 `timescale 1ns / 1ps
 
+/*
+    Module:         skid_buffer
+    Description:    pipeline register
+                    Refer to: http://fpgacpu.ca/fpga/Pipeline_Skid_Buffer.html
+
+    + means insert (handshake at input ports)
+    - mean remove (handshake at output ports)
+
+    This skid buffer has three states:
+    1. It is Empty.
+    2. It is Busy, holding one item of data in the main register, either waiting or actively transferring data through that register.
+    3. It is Full, holding data in both registers, and stopped until the main register is emptied and simultaneously refilled from the buffer register,
+       so no data is lost or reordered. (Without an available empty register, the input interface cannot skid to a stop, so it must signal it is not ready.)
+    4. It is Full and in Circular Buffer Mode, holding data in both registers, and can accept new data into the buffer register
+       while simultaneously replacing the contents of the main register with the current contents of the buffer register.
+
+    CBM means in CIRCULAR_BUFFER_MODE, always allowing insert,
+    which moves the data_buff_reg to data_out_reg, and load new inserted data to data_buff_reg
+
+
+                    /--\ +- flow
+                    |  |
+            load   |  v   fill
+    -------   +    ------   +    ------        (CBM)
+    |       | ---> |      | ---> |      | ---\ +  dump
+    | Empty |      | Busy |      | Full |    |   or
+    |       | <--- |      | <--- |      | <--/ +- pass
+    -------    -   ------    -   ------
+            unload         flush
+*/
+
 module skid_buffer #(
-    parameter int DATA_WIDTH          = 8,
-    parameter int CICULAR_BUFFER_MODE = 0
+    parameter int DATA_WIDTH           = 8,
+    parameter int CIRCULAR_BUFFER_MODE = 0
 ) (
     input  logic                  clk,
     input  logic                  rst,
@@ -15,5 +46,147 @@ module skid_buffer #(
     output logic                  valid_out,
     input  logic                  ready_out
 );
+    localparam bit [DATA_WIDTH-1:0] DataZero = {DATA_WIDTH{1'b0}};
+    /*
+    Data path
+        Registers:
+            data_buffer_reg can receive data_in and output to data_out_reg.
+            data_out_reg receives selected_data and outputs to data_out.
+        Control signals:
+            data_buffer_wren enables write data_in to data_buffer_reg.
+            data_out_wren enables write selected_data to data_out_reg.
+            use_buffered_data is high if data_buffer_out is selected to sent to data_out_reg, else data_in is sent.
+    */
+    logic                  data_buffer_wren;  // EMPTY at start, so don't load.
+    logic [DATA_WIDTH-1:0] data_buffer_out;
+
+    register_slice #(
+        .DATA_WIDTH (DATA_WIDTH),
+        .RESET_VALUE(DataZero)
+    ) data_buffer_reg (
+        .clk     (clk),
+        .clk_en  (data_buffer_wren),
+        .rst     (rst),
+        .data_in (data_in),
+        .data_out(data_buffer_out)
+    );
+
+    logic                  data_out_wren;  // EMPTY at start, so accept data.
+    logic                  use_buffered_data;
+    logic [DATA_WIDTH-1:0] selected_data;
+
+    always_comb begin : selected_data_logic
+        selected_data = (use_buffered_data == 1'b1) ? data_buffer_out : data_in;
+    end
+
+    register_slice #(
+        .DATA_WIDTH (DATA_WIDTH),
+        .RESET_VALUE(DataZero)
+    ) data_out_reg (
+        .clk     (clk),
+        .clk_en  (data_out_wren),
+        .rst     (rst),
+        .data_in (selected_data),
+        .data_out(data_out)
+    );
+
+    /*
+    Control path
+    */
+
+    // FSM
+    localparam int StateBits = 2;
+    typedef enum bit [StateBits-1:0] {
+        EMPTY,
+        BUSY,
+        FULL
+    } state_t;
+
+    state_t state_cur, state_next;
+    register_slice #(
+        .DATA_WIDTH (StateBits),
+        .RESET_VALUE(EMPTY)       // initial state is EMPTY
+    ) state_reg (
+        .clk     (clk),
+        .clk_en  (1'b1),
+        .rst     (rst),
+        .data_in (state_next),
+        .data_out(state_cur)
+    );
+
+    // handshake
+    logic insert, remove;
+
+    always_comb begin : insert_remove_logic
+        insert = (valid_in & ready_in);
+        remove = (valid_out & ready_out);
+    end
+
+    // data path states
+    logic load, flow, fill, unload, flush, dump, pass;
+    always_comb begin : datapath_state_logic
+        load   = (state_cur == EMPTY) && (insert == 1'b1) && (remove == 1'b0);
+        flow   = (state_cur == BUSY) && (insert == 1'b1) && (remove == 1'b1);
+        fill   = (state_cur == BUSY) && (insert == 1'b1) && (remove == 1'b0);
+        unload = (state_cur == BUSY) && (insert == 1'b0) && (remove == 1'b1);
+        flush  = (state_cur == FULL) && (insert == 1'b0) && (remove == 1'b1);
+        dump   = (state_cur == FULL) && (insert == 1'b1) && (remove == 1'b0) && (CIRCULAR_BUFFER_MODE == 1);
+        pass   = (state_cur == FULL) && (insert == 1'b1) && (remove == 1'b1) && (CIRCULAR_BUFFER_MODE == 1);
+    end
+
+    // this FSM doesn't handle illegal state transfers
+    always_comb begin : fsm_next_state_logic
+        state_next = (load == 1'b1) ? BUSY : state_cur;
+        state_next = (flow == 1'b1) ? BUSY : state_next;
+        state_next = (fill == 1'b1) ? FULL : state_next;
+        state_next = (flush == 1'b1) ? BUSY : state_next;
+        state_next = (unload == 1'b1) ? EMPTY : state_next;
+        state_next = (dump == 1'b1) ? FULL : state_next;
+        state_next = (pass == 1'b1) ? FULL : state_next;
+    end
+
+    // control signals to datapath: data_buffer_wren, data_out_wren, use_buffered_data
+
+    always_comb begin : datapath_control_signal_logic
+        data_buffer_wren  = (fill == 1'b1) || (dump == 1'b1) || (pass == 1'b1);
+        data_out_wren     = (load == 1'b1) || (flow == 1'b1) || (dump == 1'b1) || (pass == 1'b1) || (flush == 1'b1);
+        use_buffered_data = (flush == 1'b1) || (dump == 1'b1) || (pass == 1'b1);
+    end
+
+    /*
+    Output signals
+        - ready_in
+        - valid_out
+        ready_in and valid_out are stored in registers,
+        which avoids directly comb logic across two pipeline stages
+    */
+    logic ready_in_next;
+
+    // ready_in is stored in a register
+    register_slice #(
+        .DATA_WIDTH (1),
+        .RESET_VALUE(1'b1)
+    ) ready_in_reg (
+        .clk     (clk),
+        .clk_en  (1'b1),
+        .rst     (rst),
+        .data_in (ready_in_next),
+        .data_out(ready_in)
+    );
+    assign ready_in_next = ((state_next != FULL) || (CIRCULAR_BUFFER_MODE == 1));
+
+    // valid_out is stored in a register
+    logic valid_out_next;
+    register_slice #(
+        .DATA_WIDTH (1),
+        .RESET_VALUE(1'b0)
+    ) valid_out_reg (
+        .clk     (clk),
+        .clk_en  (1'b1),
+        .rst     (rst),
+        .data_in (valid_out_next),
+        .data_out(valid_out)
+    );
+    assign valid_out_next = (state_next != EMPTY);
 
 endmodule
---
+++ hardware/user/components/common/test/skid_buffer_tb.py
@@ -0,0 +1,219 @@
+from random import randint
+import cocotb
+from cocotb import triggers as cc_triggers
+from cocotb.utils import get_sim_time
+from lqer_cocotb import Testbench, lqer_runner
+from lqer_cocotb.interface import StreamDriver, StreamMonitor, bit_driver
+from lqer_cocotb.utils import signal_integer
+
+
+class SkidBufferTB(Testbench):
+    def __init__(self, dut, enable_driver: bool) -> None:
+        super().__init__(dut, dut.clk, dut.rst)
+        self.assign_self_params("DATA_WIDTH", "CIRCULAR_BUFFER_MODE")
+
+        self.DATA_IN_MIN = 0
+        self.DATA_IN_MAX = 2**self.DATA_WIDTH - 1
+
+        self.EMPTY = 0
+        self.BUSY = 1
+        self.FULL = 2
+
+        if enable_driver:
+            self.input_driver = StreamDriver(dut.clk, dut.data_in, dut.valid_in, dut.ready_in)
+            self.output_monitor = StreamMonitor(dut.clk, dut.data_out, dut.valid_out, dut.ready_out)
+
+    def generate_inputs(self, random: bool):
+        if not random:
+            return self.DATA_IN_MAX
+        else:
+            return randint(self.DATA_IN_MIN, self.DATA_IN_MAX)
+
+    def model(self, data_in):
+        return data_in
+
+
+def generate_random_data_widths():
+    params = {"DATA_WIDTH": randint(1, 64)}
+    return params
+
+
+def check_msg(name: str):
+    return f"{name} check failed at {get_sim_time('ns')}"
+
+
+@cocotb.test()
+async def check_rst(dut):
+    tb = SkidBufferTB(dut, enable_driver=False)
+    dut.ready_in.value = 1
+    await tb.reset()
+    assert signal_integer(dut.state_cur) == tb.EMPTY, check_msg("rst")
+
+
+@cocotb.test()
+async def check_control_path(dut):
+    """
+    Check the control flow FSM states transfer of skid_buffer
+
+    This test goes through the following states counterclockwise:
+    Empty - load -> Busy - flow -> Busy - fill -> Full [- dump -> Full - pass -> Full] - flush -> Busy - flow -> Busy - unload -> Empty
+
+    ```txt
+                    /--\ +- flow
+                    |  |
+            load   |  v   fill
+    -------   +    ------   +    ------        (CBM)
+    |       | ---> |      | ---> |      | ---\ +  dump
+    | Empty |      | Busy |      | Full |    |   or
+    |       | <--- |      | <--- |      | <--/ +- pass
+    -------    -   ------    -   ------
+            unload         flush
+    ```
+    """
+    tb = SkidBufferTB(dut, enable_driver=False)
+    dut.valid_in.value = 1
+    dut.ready_out.value = 1
+    dut.data_in.value = tb.DATA_IN_MAX
+    await tb.reset()
+    tb.log_sim_time("check_control_path reset")
+    # load
+    assert signal_integer(dut.load) == 1, check_msg("data path state 'load'")
+    # EMPTY -> BUSY
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.BUSY, check_msg("FSM state 'BUSY'")
+    # flow
+    assert signal_integer(dut.flow) == 1, check_msg("data path state 'flow'")
+    # BUSY -> BUSY
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.BUSY, check_msg("FSM state 'BUSY'")
+    # fill
+    dut.ready_out.value = 0
+    await cc_triggers.ReadOnly()
+    assert signal_integer(dut.fill) == 1, check_msg("data path state 'fill'")
+    # BUSY -> FULL
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.FULL, check_msg("FSM state 'FULL'")
+    if tb.CIRCULAR_BUFFER_MODE:
+        # dump
+        assert signal_integer(dut.dump) == 1, check_msg("data path state 'dump'")
+        # FULL -> FULL
+        await cc_triggers.FallingEdge(dut.clk)
+        assert signal_integer(dut.state_cur) == tb.FULL, check_msg("FSM state 'FULL'")
+        "Now valid_in should still be 1"
+        dut.ready_out.value = 1
+        await cc_triggers.ReadOnly()
+        # pass
+        assert signal_integer(getattr(dut, "pass")) == 1, check_msg("data path state 'pass'")
+        # FULL -> FULL
+        await cc_triggers.FallingEdge(dut.clk)
+        assert signal_integer(dut.state_cur) == tb.FULL, check_msg("FSM state 'FULL'")
+    else:
+        "Now valid_in should be 0"
+    dut.valid_in.value = 0
+    dut.ready_out.value = 1
+    # flush
+    await cc_triggers.ReadOnly()
+    assert signal_integer(dut.flush) == 1, check_msg("data path state 'flush'")
+    # FULL -> BUSY
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.BUSY, check_msg("FSM state 'BUSY'")
+    dut.valid_in.value = 1
+    dut.ready_out.value = 1
+    # fill
+    await cc_triggers.ReadOnly()
+    assert signal_integer(dut.flow) == 1, check_msg("data path state 'flow'")
+    # BUSY -> BUSY
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.BUSY, check_msg("FSM state 'BUSY'")
+    dut.valid_in.value = 0
+    dut.ready_out.value = 1
+    # unload
+    await cc_triggers.ReadOnly()
+    assert signal_integer(dut.unload) == 1, check_msg("data path state 'unload'")
+    # BUSY -> EMPTY
+    await cc_triggers.FallingEdge(dut.clk)
+    assert signal_integer(dut.state_cur) == tb.EMPTY, check_msg("FSM state 'EMPTY'")
+
+
+@cocotb.test()
+async def check_data_path_no_back_pressure(dut):
+    NUM_TRANSACTIONS = 100
+    tb = SkidBufferTB(dut, enable_driver=True)
+    await tb.reset()
+    tb.log_sim_time("check_data_path_no_beck_pressure reset")
+    tb.output_monitor.ready.value = 1
+    tb.input_driver.set_valid_prob(0.5)
+
+    for _ in range(NUM_TRANSACTIONS):
+        data_in = tb.generate_inputs(random=True)
+        expect_out = tb.model(data_in)
+        tb.input_driver.append(data_in)
+        tb.output_monitor.expect(expect_out)
+
+    await cc_triggers.Timer(100, units="us")
+    assert tb.output_monitor.exp_queue.empty()
+
+
+@cocotb.test()
+async def check_data_path_CBM(dut):
+    if dut.CIRCULAR_BUFFER_MODE == 0:
+        return
+    tb = SkidBufferTB(dut, enable_driver=False)
+    await tb.reset()
+    data_in_0 = tb.generate_inputs(random=True)
+    dut.data_in.value = data_in_0
+    dut.valid_in.value = 1
+    dut.ready_out.value = 0
+    tb.log_sim_time("check_data_path_CBM")
+    if tb.CIRCULAR_BUFFER_MODE:
+        await cc_triggers.FallingEdge(dut.clk)  # BUSY
+        await cc_triggers.FallingEdge(dut.clk)  # FULL
+        assert signal_integer(dut.state_cur) == tb.FULL, check_msg("FSM state 'FULL'")
+        assert signal_integer(dut.data_out) == data_in_0, check_msg("data path 'data_out'")
+        data_in_1 = tb.generate_inputs(random=True)
+        dut.data_in.value = data_in_1
+        await cc_triggers.FallingEdge(dut.clk)
+        assert signal_integer(dut.data_out) == data_in_0
+        assert signal_integer(dut.data_buffer_out) == data_in_1
+    else:
+        tb.log_sim_time("Skip check_data_path_circular_buffer_mode (CIRCULAR_BUFFER_MODEl==0)")
+        pass
+
+
+@cocotb.test()
+async def check_data_path_back_pressure_no_CBM(dut):
+    NUM_ITERATIONS = 100
+    if dut.CIRCULAR_BUFFER_MODE == 1:
+        return
+    tb = SkidBufferTB(dut, enable_driver=True)
+    cocotb.start_soon(bit_driver(dut.ready_out, clk=dut.clk, prob=0.5))
+    tb.input_driver.set_valid_prob(0.8)
+    await tb.reset()
+    tb.log_sim_time("check_data_path_back_pressure_no_CBM reset")
+
+    for _ in range(NUM_ITERATIONS):
+        data_in = tb.generate_inputs(random=True)
+        expect_out = tb.model(data_in)
+        tb.input_driver.append(data_in)
+        tb.output_monitor.expect(expect_out)
+    await cc_triggers.Timer(100, units="us")
+    assert tb.output_monitor.exp_queue.empty()
+
+
+def pytest_skid_buffer():
+    param_list = [
+        {"DATA_WIDTH": 1, "CIRCULAR_BUFFER_MODE": 0},
+        {"DATA_WIDTH": 1, "CIRCULAR_BUFFER_MODE": 1},
+        {"DATA_WIDTH": 8, "CIRCULAR_BUFFER_MODE": 0},
+        {"DATA_WIDTH": 8, "CIRCULAR_BUFFER_MODE": 1},
+    ]
+
+    for _ in range(10):
+        param_list.append(generate_random_data_widths() | {"CIRCULAR_BUFFER_MODE": 0})
+        param_list.append(generate_random_data_widths() | {"CIRCULAR_BUFFER_MODE": 1})
+
+    lqer_runner(param_list)
+
+
+if __name__ == "__main__":
+    pytest_skid_buffer()
